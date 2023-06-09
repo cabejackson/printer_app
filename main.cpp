76b48b6cd196151aaac8175e3410cce1a4becaaa
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2023-06-08 20:00:41 -0600

    Add printer class with constructor

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Printer {
+    string _name;
+    int _availablePaper;
+public:
+    Printer(string name, int availablePaper){
+        _name = name;
+        _availablePaper = availablePaper;
+    }
+};
+
+int main(){
+
+    return 0;
+}
